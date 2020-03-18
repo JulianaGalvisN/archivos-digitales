222ba907184395155df4f15d80f27e52bd17e7ef
juliana <julianagalvisn@gmail.com>
2020-03-18 18:32:33 -0500

    trying to fix bug in navbar

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -6,12 +6,12 @@ function Navbar(props) {
   return (
 
     <div className="topnav">
-      <Link to="/" className="active">Inicio</Link>
-      <Link to="/about">Este manual</Link>
-      <Link to="/marcos-de-referencia">Marcos de referencia</Link>
-      <Link to="/archivos">Archivos</Link>
-      <Link to="/activismo">Activismo</Link>
-      <Link to="/metodologia">Metodología</Link>
+      <Link to="/" className="active link">Inicio</Link>
+      <Link to="/about" className="link">Este manual</Link>
+      <Link to="/marcos-de-referencia" className="link">Marcos de referencia</Link>
+      <Link to="/archivos" className="link">Archivos</Link>
+      <Link to="/activismo" className="link">Activismo</Link>
+      <Link to="/metodologia" className="link">Metodología</Link>
 
     </div>
   )
