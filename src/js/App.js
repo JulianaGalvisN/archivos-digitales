1d01a84c3f8602381181f1564ad36a64cd7cebe3
juliana <julianagalvisn@gmail.com>
2020-03-18 20:03:22 -0500

    added bottom banner in home

--- src/js/App.js
+++ src/js/App.js
@@ -3,8 +3,10 @@ import Banner from './Banner';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import '../css/reset.css';
+import '../css/App.css'
 import linea_tiempo from '../images/linea_tiempo.png'
 import archivo from '../images/archivo.png'
+import activismo_img from '../images/activismo-home.png'
 
 
 function App() {
@@ -18,6 +20,28 @@ function App() {
       link="/archivos"
       bgImage={archivo}
       height="455px"/>
+
+      <div className="bottom">
+        <div className="image">
+          <img src={activismo_img} alt="Imagen de activismo de género"/>
+        </div>
+        <div className="text">
+          <div>
+            <h2 className="title">
+            El activismo
+            </h2>
+            <h2 className="title">
+            de género
+            </h2>
+          </div>
+          <p className="paragraph">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt utlaoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ulla- mcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+          <p className="paragraph">
+          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feu giat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+          </p>
+        </div>
+      </div>
+
 
       <Banner
       title1="Explora la línea"
