572f546611e0dc9e156ee026cd2c6f3bfd6f4705
marianaviro <m.villamizar564@uniandes.edu.co>
2020-04-02 10:29:57 -0500

    Trying to set up home design

--- src/js/App.js
+++ src/js/App.js
@@ -14,38 +14,24 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-
-      <div className="top">
-        <div className="imagetop">
-          <img src={cano} alt="Imagen de María Cano"/>
-        </div>
-        <div className="texttop">
-          <div className="headlinetop">
-              <h2 className="title3">
-              Archivos
-              </h2>
-              <h2 className="title4">
-              digitales
-              </h2>
-          <div className="headline2">
-            <h2 className="title5">
-            para activistas de género
-            </h2>
+      <div className="home">
+        <img className="image" src={cano} alt="Imagen de María Cano"/>
+        <div className="headline-container">
+          <div className="headline">
+            <h1 className="title">Archivos</h1>
+            <h1 className="title">digitales</h1>
+            <h6 className="title subtitle">para activistas de género</h6>
           </div>
-          <div className="descriptiontop">
-            <h2 className="titledescriptiontop">
-            ¿Qué es?
-            </h2>
-            <div className="textdescription">
-              <p>
-              Este manual se crea en el marco de un proyecto de investigación de maestría en Humanidades Digitales de la Universidad de Los Andes. Consiste en brindar recursos sobre el archivo digital existente en Colombia y su posible uso por parte de activistas de género en Colombia.
-              </p>
-              <p>
-              Contiene un marco de referencia con los conceptos trabajados en el proyecto, un marco legal colombiano y un paso a paso de cómo pedir información. A su vez da a conocer el archivo digital existente en Colombia, y la información contenida en ellos que posiblemente sea útil para activistas de género.
-              </p>
+          <div className="intro text">
+            <h2 className="title">¿Qué es?</h2>
+            <div className="paragraph">
+              <p>Este manual se crea en el marco de un proyecto de investigación de maestría en Humanidades Digitales de la Universidad de Los Andes.
+              Consiste en brindar recursos sobre el archivo digital existente en Colombia y su posible uso por parte de activistas de género en Colombia.</p>
+              <p>Contiene un marco de referencia con los conceptos trabajados en el proyecto, un marco legal colombiano y un paso a paso de cómo
+              pedir información. A su vez da a conocer el archivo digital existente en Colombia, y la información contenida en ellos que posiblemente
+              sea útil para activistas de género.</p>
             </div>
           </div>
-        </div>
         </div>
       </div>
 
@@ -56,18 +42,14 @@ function App() {
       bgImage={archivo}
       height="455px"/>
 
-      <div className="bottom">
+      <div className="activism">
         <div className="image">
           <img src={activismo_img} alt="Imagen de activismo de género"/>
         </div>
         <div className="text">
-          <div className="headline">
-            <h2 className="title1">
-            El activismo
-            </h2>
-            <h2 className="title2">
-            de género
-            </h2>
+          <div className="title-container">
+            <h2 className="title">El activismo</h2>
+            <h2 className="title">de género</h2>
           </div>
           <p className="paragraph">
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt utlaoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ulla- mcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
