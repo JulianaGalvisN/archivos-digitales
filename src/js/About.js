9209f99272fde02b15fbdc55763d97286fd95e98
juliana <julianagalvisn@gmail.com>
2020-04-07 09:05:04 -0500

    added content to About

--- src/js/About.js
+++ src/js/About.js
@@ -28,8 +28,30 @@ function About(props) {
           sea útil para activistas de género.</p>
         </div>
       </div>
-    </div>
 
+      <div className="headline-container">
+        <div className="headline">
+          <h2 className="title">Para qué se hace</h2>
+          <div className="paragraph">
+            <p>El objetivo de este proyecto es brindar información y herramientas abiertas y verídicas sobre archivos digitales para que el activismo de género continúe proponiendo cambios en el orden social del país.</p>
+            <p>Esto puesto que el archivo es un potencial generador de espacios públicos y abiertos al acceso a la información, pero ha sido excluyente de ciertas identidades no tomadas en cuenta en la historia de las sociedades, como lo son las mujeres y LGBT. Es por eso que para que dichas identidades tengan una voz y puedan luchar por sus derechos, el activismo en temas de género debe preservar su patrimonio a través de la entrada en espacios de recolección y divulgación de información como lo son los archivos digitales.</p>
+            <p>Es por eso que el llamado de esta investigación con énfasis en activismo de género y de archivo, parte de la práctica de la historia a través de la producción de archivo y la importancia que ha supuesto la inclusión de quienes han sido excluidos de la historia y el patrimonio de las sociedades. Y más aún, busca crear formas de relacionamiento de este activismo, en lucha constante por la inclusión, con la producción histórica y patrimonial ofrecida por los archivos digitales.</p>
+            <p>A su vez, busca contribuir a la lucha por el acceso a la información pública que hace parte fundamental de la activa participación de la ciudadanía en diversos temas públicos</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="headline">
+          <h2 className="title">¿Por qué es importante</h2>
+          <h2 className="title">hablar de archivos digitales?</h2>
+          <div className="paragraph">
+            <p>El objetivo de este proyecto es brindar información y herramientas abiertas y verídicas sobre archivos digitales para que el activismo de género continúe proponiendo cambios en el orden social del país.</p>
+            <p>Esto puesto que el archivo es un potencial generador de espacios públicos y abiertos al acceso a la información, pero ha sido excluyente de ciertas identidades no tomadas en cuenta en la historia de las sociedades, como lo son las mujeres y LGBT. Es por eso que para que dichas identidades tengan una voz y puedan luchar por sus derechos, el activismo en temas de género debe preservar su patrimonio a través de la entrada en espacios de recolección y divulgación de información como lo son los archivos digitales.</p>
+            <p>Es por eso que el llamado de esta investigación con énfasis en activismo de género y de archivo, parte de la práctica de la historia a través de la producción de archivo y la importancia que ha supuesto la inclusión de quienes han sido excluidos de la historia y el patrimonio de las sociedades. Y más aún, busca crear formas de relacionamiento de este activismo, en lucha constante por la inclusión, con la producción histórica y patrimonial ofrecida por los archivos digitales.</p>
+            <p>A su vez, busca contribuir a la lucha por el acceso a la información pública que hace parte fundamental de la activa participación de la ciudadanía en diversos temas públicos</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
