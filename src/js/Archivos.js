a9d6df217cb11b0d14ec7ecbb8e69499e0b594eb
David Daza <03dazal@gmail.com>
2020-04-09 13:17:28 -0500

    init filters

--- src/js/Archivos.js
+++ src/js/Archivos.js
@@ -1,96 +1,117 @@
-import React from 'react'
+import React from 'react';
 import Navbar from './Navbar';
 import Banner from './Banner';
 import Footer from './Footer';
 import Archivo from './Archivo';
 import Dropdown from './Dropdown';
-import '../css/Archivos.css'
-import archivo from '../images/banner-archivos.png'
-import lupa from '../images/lupa.svg'
+import '../css/Archivos.css';
+import archivo from '../images/banner-archivos.png';
+import lupa from '../images/lupa.svg';
 
-function Archivos(props) {
-  return (
-    <div className="archivos">
-      <Navbar/>
+// Se pasa a un componente de clase
+// En componentDidMount se lee la base de datos
+// Los archivos se filtran por tags en la funcion filterArchives
 
-      <Banner
-      title1="Los"
-      title2="archivos"
-      bgImage={archivo}
-      height="300px"
-      margin="Banner margin"/>
+class Archivos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      tags: null,
+      selectedTag: '',
+      archives: null,
+    };
+  }
 
-      <div className="filtros">
-        {/* Estos son los dropdowns pero toca modificarlos en el archivo Dropdown.js porque no tienen funcionalidad aún */}
-        <Dropdown name="Tema" options="Todos"/>
-        <Dropdown name="Tipo" options="Todos"/>
-        <div className="searchbar">
-          <p className="name">Búsqueda</p>
-          <div className="bar">
-            <input type="text" id="search" name="search"/>
-            <div className="icon">
-              <img className="image" src={lupa} alt="Ícono de lupa"/>
+  componentDidMount() {
+    // Esto es de React
+    fetch(
+      'https://script.googleusercontent.com/a/macros/randommonkey.io/echo?user_content_key=r_LlTdFIjSox2kwx0c-SHFJl8b_USSoUsov4azu48CN4hTM-GRYyNXHE84bYyEJ1FzBbf6a0g36u66YQmLrTN0ij7ra88Ui0m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP_F5M49av5jvMSEjbmACM01ubd_SaoYEJGgJfG4ynUykGlTovx4MdMsA_WKI32lApbywmIdVFRtWnWm9nNiWBhNB67k26AQb74G48i-EPPuBtz9Jw9Md8uu&lib=MGUgqta0UWIajlw18Y2HXqcPNf_ntfMqG'
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState((state) => ({
+          data: res.data,
+          tags: res.data['diccionario-tags'],
+          archives: res.data.archivo_existente.filter((archive) => {
+            return archive.tags.includes(state.selectedTag);
+          }),
+        }));
+      });
+  }
+
+  filterArchives = (event) => {
+    // Esto es propio
+    const value = event.target.value;
+    this.setState((state) => ({
+      selectedTag: value,
+      archives: state.data.archivo_existente.filter((archive) => {
+        return archive.tags.includes(value);
+      }),
+    }));
+  };
+
+  render() {
+    return (
+      <div className="archivos">
+        <Navbar />
+
+        <Banner
+          title1="Los"
+          title2="archivos"
+          bgImage={archivo}
+          height="300px"
+          margin="Banner margin"
+        />
+
+        <div className="filtros">
+          {/* Estos son los dropdowns pero toca modificarlos en el archivo Dropdown.js porque no tienen funcionalidad aún */}
+          {/* <Dropdown name="Tema" options="Todos" />
+          <Dropdown name="Tipo" options="Todos" /> */}
+          <select onChange={this.filterArchives}>
+            <option value="">Todas</option>
+            {this.state.tags &&
+              this.state.tags.map((tag) => (
+                <option value={tag.uid}>{tag.nombre}</option>
+              ))}
+          </select>
+          <div className="searchbar">
+            <p className="name">Búsqueda</p>
+            <div className="bar">
+              <input type="text" id="search" name="search" />
+              <div className="icon">
+                <img className="image" src={lupa} alt="Ícono de lupa" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="results">
-        <h2 className="title-results">Resultados</h2>
-        <div className="archivos-container">
-
-          <Archivo
-          title="Lorem ipsum dolor sit amet, consec tetuer"
-          author="Lorem ipsum"
-          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam."
-          link="/archivos"/>
-
-          <Archivo
-          title="Lorem ipsum dolor sit amet, consec tetuer"
-          author="Lorem ipsum"
-          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam."
-          link="/archivos"/>
-
-          <Archivo
-          title="Lorem ipsum dolor sit amet, consec tetuer"
-          author="Lorem ipsum"
-          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam."
-          link="/archivos"/>
-
-          <Archivo
-          title="Lorem ipsum dolor sit amet, consec tetuer"
-          author="Lorem ipsum"
-          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam."
-          link="/archivos"/>
-
-          <Archivo
-          title="Lorem ipsum dolor sit amet, consec tetuer"
-          author="Lorem ipsum"
-          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam."
-          link="/archivos"/>
-
-          <Archivo
-          title="Lorem ipsum dolor sit amet, consec tetuer"
-          author="Lorem ipsum"
-          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam."
-          link="/archivos"/>
-
-          <Archivo
-          title="Lorem ipsum dolor sit amet, consec tetuer"
-          author="Lorem ipsum"
-          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam."
-          link="/archivos"/>
-
-          <Archivo
-          title="Lorem ipsum dolor sit amet, consec tetuer"
-          author="Lorem ipsum"
-          description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam."
-          link="/archivos"/>
-
+        <div className="results">
+          <h2 className="title-results">Resultados</h2>
+          {this.state.archives && (
+            <p>Viendo {this.state.archives.length} archivos</p>
+          )}
+          <div className="archivos-container">
+            {this.state.archives ? (
+              this.state.archives.map((archive) => {
+                return (
+                  <Archivo
+                    title={archive.nombre}
+                    author={archive['autor-organizacion']}
+                    description={archive.descripcion}
+                    link={archive.url}
+                    key={archive.uid}
+                  />
+                );
+              })
+            ) : (
+              <p>Cargando</p>
+            )}
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
-  )
+    );
+  }
 }
 
 export default Archivos;
