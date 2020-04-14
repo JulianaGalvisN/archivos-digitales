5068ec9ac15e64590b8892c31fd60887c9ac28f7
David Daza <03dazal@gmail.com>
2020-04-14 11:53:47 -0500

    Updates

---
+++ precache-manifest.5d6c1f2f5e5f6b88bbaeb3d4ac1fdac3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288d94b36dbe8e5bdd1c81183b8e28b1",
+    "url": "/archivos-digitales/index.html"
+  },
+  {
+    "revision": "b9b855a9e5cebf487e0b",
+    "url": "/archivos-digitales/static/css/main.5a1caa9e.chunk.css"
+  },
+  {
+    "revision": "fd5c962a594e4a059fab",
+    "url": "/archivos-digitales/static/js/2.db8b6c1f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/archivos-digitales/static/js/2.db8b6c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b855a9e5cebf487e0b",
+    "url": "/archivos-digitales/static/js/main.a817a71a.chunk.js"
+  },
+  {
+    "revision": "93329a03660c33d64260",
+    "url": "/archivos-digitales/static/js/runtime-main.80a1d64e.js"
+  },
+  {
+    "revision": "2bf0435fecceb26b1d5e88cbbe4d3c72",
+    "url": "/archivos-digitales/static/media/activismo-home.2bf0435f.png"
+  },
+  {
+    "revision": "ec9769f3a34b352c38dabc1a740739ef",
+    "url": "/archivos-digitales/static/media/activismo.ec9769f3.png"
+  },
+  {
+    "revision": "4ff35181605652e79725d53fc8380f3d",
+    "url": "/archivos-digitales/static/media/archivo.4ff35181.png"
+  },
+  {
+    "revision": "cd99750a5cfd2f616dd38fc4cc449dc9",
+    "url": "/archivos-digitales/static/media/banner-archivos.cd99750a.png"
+  },
+  {
+    "revision": "7fe043bde065e31b88e489630a34cf66",
+    "url": "/archivos-digitales/static/media/cano.7fe043bd.png"
+  },
+  {
+    "revision": "0f2b16010e23fd2cd8f9ddf6aab18566",
+    "url": "/archivos-digitales/static/media/cc.0f2b1601.svg"
+  },
+  {
+    "revision": "a4f8be0d5fdd7415d959325515a391ca",
+    "url": "/archivos-digitales/static/media/este-manual.a4f8be0d.png"
+  },
+  {
+    "revision": "58f76f297dec60e2ddf23c2848b84c7d",
+    "url": "/archivos-digitales/static/media/fb.58f76f29.svg"
+  },
+  {
+    "revision": "84116c776ff85af1d56e6cb32f410d07",
+    "url": "/archivos-digitales/static/media/hamburger.84116c77.svg"
+  },
+  {
+    "revision": "5f93011af56f7abcb9944158fc5ce541",
+    "url": "/archivos-digitales/static/media/linea_tiempo.5f93011a.png"
+  },
+  {
+    "revision": "c3f4927bdf4f31f8e9a1763bb77d7984",
+    "url": "/archivos-digitales/static/media/logo_footer.c3f4927b.svg"
+  },
+  {
+    "revision": "a857307c3b3454daa3774456a1ad838c",
+    "url": "/archivos-digitales/static/media/logo_mini.a857307c.svg"
+  },
+  {
+    "revision": "6ff7b8e00d25bc56e85fbf8c2473424f",
+    "url": "/archivos-digitales/static/media/lupa.6ff7b8e0.svg"
+  },
+  {
+    "revision": "2af67e473e8544a60b3bb4a48815cb8b",
+    "url": "/archivos-digitales/static/media/mail.2af67e47.svg"
+  },
+  {
+    "revision": "9c5e890272ec5f51036c2b4fd1d0f2d5",
+    "url": "/archivos-digitales/static/media/marcos.9c5e8902.png"
+  },
+  {
+    "revision": "fcd773a540106e19b7192ec15dee8d99",
+    "url": "/archivos-digitales/static/media/metodologia.fcd773a5.png"
+  },
+  {
+    "revision": "dc1517ea6dfc830090931e2f40d46947",
+    "url": "/archivos-digitales/static/media/tw.dc1517ea.svg"
+  }
+]);
