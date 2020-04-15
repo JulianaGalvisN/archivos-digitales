614200e8e46cd79475787f3da87e8a5d46bf2cda
David Daza <03dazal@gmail.com>
2020-04-15 18:45:36 -0500

    fix filter and search bug

--- src/js/Archivos.js
+++ src/js/Archivos.js
@@ -6,6 +6,9 @@ import '../css/Archivos.css';
 import archivo from '../images/banner-archivos.png';
 import lupa from '../images/lupa.svg';
 
+const DB_URL =
+  'https://script.googleusercontent.com/a/macros/randommonkey.io/echo?user_content_key=r_LlTdFIjSox2kwx0c-SHFJl8b_USSoUsov4azu48CN4hTM-GRYyNXHE84bYyEJ1FzBbf6a0g36u66YQmLrTN0ij7ra88Ui0m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP_F5M49av5jvMSEjbmACM01ubd_SaoYEJGgJfG4ynUykGlTovx4MdMsA_WKI32lApbywmIdVFRtWnWm9nNiWBhNB67k26AQb74G48i-EPPuBtz9Jw9Md8uu&lib=MGUgqta0UWIajlw18Y2HXqcPNf_ntfMqG';
+
 // Se pasa a un componente de clase
 // En componentDidMount se lee la base de datos
 // Los archivos se filtran por tags en la funcion filterArchives
@@ -18,32 +21,28 @@ class Archivos extends React.Component {
       tags: null,
       tag: {
         uid: '',
-        label: '',
+        label: 'Todos',
       },
       archives: null,
       query: '',
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     // Esto es de React
-    fetch(
-      'https://script.googleusercontent.com/a/macros/randommonkey.io/echo?user_content_key=r_LlTdFIjSox2kwx0c-SHFJl8b_USSoUsov4azu48CN4hTM-GRYyNXHE84bYyEJ1FzBbf6a0g36u66YQmLrTN0ij7ra88Ui0m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_nRPgeZU6HP_F5M49av5jvMSEjbmACM01ubd_SaoYEJGgJfG4ynUykGlTovx4MdMsA_WKI32lApbywmIdVFRtWnWm9nNiWBhNB67k26AQb74G48i-EPPuBtz9Jw9Md8uu&lib=MGUgqta0UWIajlw18Y2HXqcPNf_ntfMqG'
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        this.setState((state) => ({
-          data: res.data,
-          tags: res.data['diccionario-tags'],
-          archives: res.data.archivo_existente.filter((archive) => {
-            const matches = archive.nombre
-              .toLowerCase()
-              .includes(state.query.toLowerCase());
-            const hasTag = archive.tags.includes(state.tag.uid);
-            return matches && hasTag;
-          }),
-        }));
-      });
+    const response = await fetch(DB_URL);
+    const { data } = await response.json();
+    this.setState((state) => ({
+      data,
+      tags: data['diccionario-tags'],
+      archives: data.archivo_existente.filter((archive) => {
+        const matches = archive.nombre
+          .toLowerCase()
+          .includes('');
+        const hasTag = archive.tags.includes(state.tag.uid);
+        return matches && hasTag;
+      }),
+    }));
   }
 
   filterArchives = (uid, nombre) => {
@@ -57,7 +56,7 @@ class Archivos extends React.Component {
         const matches = archive.nombre
           .toLowerCase()
           .includes(state.query.toLowerCase());
-        const hasTag = archive.tags.includes(state.tag.uid);
+        const hasTag = archive.tags.includes(uid);
         return matches && hasTag;
       }),
     }));
@@ -71,7 +70,7 @@ class Archivos extends React.Component {
       archives: state.data.archivo_existente.filter((archive) => {
         const matches = archive.nombre
           .toLowerCase()
-          .includes(state.query.toLowerCase());
+          .includes(query.toLowerCase());
         const hasTag = archive.tags.includes(state.tag.uid);
         return matches && hasTag;
       }),
